8273b3b804363e19c2c56638d2d423c9edbe4781
Fabien <fab.grignoux@gmail.com>
2021-04-19 10:45:20 +0200

    Update struct

--- structPkg/struct_test.go
+++ structPkg/struct_test.go
@@ -16,12 +16,41 @@ func TestPerimeter(t *testing.T) {
 
 
 func TestArea(t *testing.T){
-	rectangle := Rectangle{4.0,3.0}
 
-	got := Area(rectangle)
-	want:=12.0
+	// checkArea:=func(t testing.TB, shape Shape, want float64){
+	// 	t.Helper()
+	// 	got := shape.Area() 
+	// 	if got!=want {
+	// 		t.Errorf("expected %g got %g",got,want)
+	// 	}
+	// }
 
-	if(got !=want){
-		t.Errorf("expected %.2f got %.2f",want, got)
+	// t.Run("Rectangles",func(t *testing.T) {
+	// 	rectangle := Rectangle{12,6}
+	// 	checkArea(t, rectangle,72.0)
+	// })
+
+
+	// t.Run("circles",func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle,314.1592653589793)
+	// })
+
+	// refacto
+	areaTests:=[]struct {
+		name string
+		shape Shape
+		want float64
+	}{
+		{name: "Rectangle",shape:Rectangle{Width:12,Height:6}, want:72},
+		{name: "Circles", shape:Circle{Radius:10}, want:314.1592653589793},
+		{name:"Triangle",shape:Triangle{Base:12, Height:6}, want: 36.0},
+	}
+
+	for _, tt:= range areaTests{
+		got:= tt.shape.Area()
+		if got!=tt.want {
+			t.Errorf("expected %g got %g",got,tt.want)
+		}
 	}
 }
